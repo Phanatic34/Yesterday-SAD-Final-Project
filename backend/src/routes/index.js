0cdc593c4bf401aea45b70fceba14d9ed709daa2
hollymolly3309 <eric03190319@gmail.com>
2026-05-25 19:36:45 +0800

    feat: merge origin

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,16 +3,12 @@ const healthController = require("../controllers/healthController");
 const authRoutes = require("./authRoutes");
 const projectRoutes = require("./projectRoutes");
 const scoreRoutes = require("./scoreRoutes");
-const historyRoutes = require("./historyRoutes");
-const sectionRoutes = require("./sectionRoutes");
 
 const router = express.Router();
 
 router.get("/health", healthController.getHealth);
 router.use("/auth", authRoutes);
-router.use("/sections", sectionRoutes);
 router.use("/projects", projectRoutes);
-router.use("/projects", historyRoutes);
 router.use("/scores", scoreRoutes);
 
 module.exports = router;
